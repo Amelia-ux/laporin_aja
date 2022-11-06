15a69cecb4946983b2d1decd8d10b69d0a57cafd
amelia <meliamel.9977@gmail.com>
2022-11-06 18:15:04 +0700

    admin views

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 //Auth::routes();
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/register', function(){
+    return view('register');
+});
 
 Route::get('/pengguna', function(){
     return view('admin/dataAnggota');
